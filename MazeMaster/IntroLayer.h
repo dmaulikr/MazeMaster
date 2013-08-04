d8c37aa4ad711cdaddadad403c569e00ff85ee72
Gregory Klein <gregoryhklein@gmail.com>
2013-08-04 19:47:30 -0400

    Initial Commit

---
+++ MazeMaster/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  MazeMaster
+//
+//  Created by Gregory Klein on 8/4/13.
+//  Copyright Binary Gods 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ MazeMaster/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MazeMaster
+//
+//  Created by Gregory Klein on 8/4/13.
+//  Copyright Binary Gods 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
