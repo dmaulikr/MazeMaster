5c4a7b1c2de1b99cd22b4f3bac1bf475e2d8dd0a
Justin Fila <justinfila@Justins-MacBook-Pro.local>
2013-08-13 21:32:56 -0400

    Added static level factory

---
+++ MazeMaster/LevelFactory.h
@@ -0,0 +1,15 @@
+//
+//  LevelFactory.h
+//  MazeMaster
+//
+//  Created by Justin Fila on 8/13/13.
+//  Copyright (c) 2013 Binary Gods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LevelFactory : NSObject
+
++(id) levelFactory;
+
+@end
---
+++ MazeMaster/LevelFactory.m
@@ -0,0 +1,39 @@
+//
+//  LevelFactory.m
+//  MazeMaster
+//
+//  Created by Justin Fila on 8/13/13.
+//  Copyright (c) 2013 Binary Gods. All rights reserved.
+//
+
+#import "LevelFactory.h"
+
+@implementation LevelFactory
+
+static LevelFactory *sharedLevelFactory = nil;
++(id) levelFactory
+{
+   if ( sharedLevelFactory == nil )
+   {
+      sharedLevelFactory = [[self alloc] init];
+   }
+   return sharedLevelFactory;
+}
+
+-(id) init
+{
+   if (self = [super init]) {
+      // initialize variables
+      NSLog(@"Initialize Variables");
+   }
+   return self;
+}
+
+-(void) dealloc
+{
+   [super dealloc];
+   
+   NSLog(@"dealloc");
+}
+
+@end
