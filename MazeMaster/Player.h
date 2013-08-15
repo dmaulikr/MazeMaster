b56b7282f4f2b45502b4dea34010b58c64128c22
Gregory Klein <gregoryhklein@gmail.com>
2013-08-14 23:55:06 -0400

    Added files for Player class

---
+++ MazeMaster/Player.h
@@ -0,0 +1,16 @@
+//
+//  Player.h
+//  MazeMaster
+//
+//  Created by Gregory Klein on 8/14/13.
+//  Copyright 2013 Binary Gods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Player : CCLayer {
+    
+}
+
+@end
---
+++ MazeMaster/Player.m
@@ -0,0 +1,14 @@
+//
+//  Player.m
+//  MazeMaster
+//
+//  Created by Gregory Klein on 8/14/13.
+//  Copyright 2013 Binary Gods. All rights reserved.
+//
+
+#import "Player.h"
+
+
+@implementation Player
+
+@end
