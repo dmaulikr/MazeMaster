ad024523e6a038de61c25c5427cd5d80b8dc6d02
Justin Fila <justinfila@Justins-MacBook-Pro.local>
2013-08-19 23:34:08 -0500

    added player sprite class

---
+++ MazeMaster/Player.h
@@ -0,0 +1,16 @@
+//
+//  Player.h
+//  MazeMaster
+//
+//  Created by Justin Fila on 8/19/13.
+//  Copyright 2013 Binary Gods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Player : CCSprite {
+    
+}
+
+@end
---
+++ MazeMaster/Player.m
@@ -0,0 +1,14 @@
+//
+//  Player.m
+//  MazeMaster
+//
+//  Created by Justin Fila on 8/19/13.
+//  Copyright 2013 Binary Gods. All rights reserved.
+//
+
+#import "Player.h"
+
+
+@implementation Player
+
+@end
