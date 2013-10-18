d3c26af05c8b32ee907dd931c4ab48eac1f9e9ca
Gregory Klein <gregoryhklein@gmail.com>
2013-10-18 00:58:02 -0400

    Added PathFinder class

---
+++ MazeMaster/PathFinder.h
@@ -0,0 +1,25 @@
+//
+//  PathFinder.h
+//  MazeMaster
+//
+//  Created by Gregory Klein on 10/18/13.
+//  Copyright (c) 2013 Binary Gods. All rights reserved.
+//
+
+#ifndef __MazeMaster__PathFinder__
+#define __MazeMaster__PathFinder__
+
+#include <iostream.h>
+
+@class Tile;
+class PathFinder
+{
+ public:
+   PathFinder();
+   ~PathFinder();
+
+   void getPathToTile(Tile *tile);
+ private:
+};
+
+#endif /* defined(__MazeMaster__PathFinder__) */
---
+++ MazeMaster/PathFinder.mm
@@ -0,0 +1,25 @@
+//
+//  PathFinder.cpp
+//  MazeMaster
+//
+//  Created by Gregory Klein on 10/18/13.
+//  Copyright (c) 2013 Binary Gods. All rights reserved.
+//
+
+#include "PathFinder.h"
+#import "Tile.h"
+
+PathFinder::PathFinder()
+{
+   std::cout << "constructing PathFinder" << std::endl;
+}
+
+PathFinder::~PathFinder()
+{
+   std::cout << "destructing PathFinder" << std::endl;
+}
+
+void PathFinder::getPathToTile(Tile *tile)
+{
+   std::cout << "getting path!" << std::endl;
+}
